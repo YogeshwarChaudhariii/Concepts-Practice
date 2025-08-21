791463f95643a9555eb2e436a22bb2cc9e39a2ff
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2025-08-21 12:41:29 +0530

    Add files via upload

---
+++ C Language/Structure2.c
@@ -0,0 +1,70 @@
+/*
+Create a structure Student with roll no, name and marks.
+Input and display details of 5 students.
+*/
+
+#include<stdio.h>
+
+# define SIZE 2
+
+struct Student
+{
+    int RollNo;
+    char Name[50];
+    float Marks;
+};
+
+// struct Student student[1] =
+// {
+
+//     {87, "Yogeshwar", 80.37f}
+// };
+
+void InputDetails(struct Student student[], int size)
+{
+    int i = 0;
+
+    for ( i = 0; i < size; i++)
+    {
+        printf("Enter details for Student %d: \n", i+1);
+        printf("Roll Number: ");
+        scanf("%d",&student[i].RollNo);
+
+        printf("Name: ");
+        scanf("%s",student[i].Name);
+
+        printf("Marks: ");
+        scanf("%f",&student[i].Marks);
+
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void DisplayDetails(struct Student student[], int size)
+{
+    int i;
+
+    printf("Student Details...\n\n");
+
+    for ( i = 0; i < size; i++)
+    {
+        printf("Student %d\n", i+1);
+        printf("Roll Number: %d\n", student[i].RollNo);
+        printf("Name: %s\n", student[i].Name);
+        printf("Marks:  %.2f\n", student[i].Marks);
+        
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    struct Student student[SIZE];
+
+    InputDetails(student, SIZE);
+    DisplayDetails(student, SIZE);
+    
+    return 0;
+}
