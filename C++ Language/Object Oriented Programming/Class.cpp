7c86e354f9bf59256fa0285e0af9a64c7b2ab31a
Yogeshwar <yogeshwarprakashchaudhari@gmail.com>
2025-11-12 22:17:42 +0530

    Updated Class.cpp and added executable

--- C++ Language/Object Oriented Programming/Class.cpp
+++ C++ Language/Object Oriented Programming/Class.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    public:
+        string name;
+        int age;
+
+        void displayInfo()
+        {
+            cout << "Name: " << name << endl;
+            cout << "Age: " << age << endl;
+        }
+};
+
+int main()
+{
+    Student student1;
+    student1.name = "Alice";
+    student1.age = 20;
+
+    student1.displayInfo();
+
+    return 0;
+}
+
 
