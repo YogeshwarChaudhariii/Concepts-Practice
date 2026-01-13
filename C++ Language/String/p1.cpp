ed78a3d3a69045526692b3867281fe837c94e9ff
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-13 11:11:12 +0530

    Create p1.cpp

---
+++ C++ Language/String/p1.cpp
@@ -0,0 +1,16 @@
+// Print string
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char str[10];
+
+    cout<<"Enter your string: "<<endl;
+    cin>>str;
+
+    cout<<str;
+    
+    return 0;
+}
