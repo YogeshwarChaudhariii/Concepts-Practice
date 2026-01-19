ae039e053db11c299cf295383cddfb01b36f4dea
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-20 00:35:54 +0530

    Create p1.java

---
+++ Java/Thread/p1.java
@@ -0,0 +1,14 @@
+class MyThread extends Thread
+{
+    public void run()
+    {
+        System.out.println("Thread is running");
+    }
+}
+
+public class p1 {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();     // Creates new thread
+    }
+}
