2e41aa662d653e3b642b0fe2f95bb3281a61e019
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-20 00:07:00 +0530

    Create p1.java

---
+++ Java/String/p1.java
@@ -0,0 +1,16 @@
+// Print name
+
+import java.util.*;
+
+class p1
+{
+    public static void main(String[] args) {
+        
+    Scanner sobj = new Scanner(System.in);
+
+    System.out.println("Enter your name: ");
+    String Name = sobj.nextLine();
+
+    System.out.println("Your name is: "+Name);
+    }
+}
