eff2954aed964429973b3113e73427f81f58358c
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-19 20:18:08 +0530

    Create Array1.java

---
+++ Java/Array/Array1.java
@@ -0,0 +1,14 @@
+public class Array1 
+{
+    public static void main(String args[])
+    {
+        int Arr[] = {10, 20, 30, 40};
+
+        System.out.println(Arr[0]);
+        System.out.println(Arr[1]);
+        System.out.println(Arr[2]);
+        System.out.println(Arr[3]);
+
+        System.out.println(Arr.length);
+    }
+}
