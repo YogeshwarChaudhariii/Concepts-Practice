0ee89268321d18a26b8b502e06ac5818a1b51132
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-24 01:21:18 +0530

    Create p1.c

---
+++ C Language/Memory Allocation/Dynamic/p1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *ptr = NULL;
+    int iSize = 0;
+
+    printf("Enter number of elements that you want to allocate: \n");
+    scanf("%d",&iSize);
+
+    ptr = (int *)malloc(iSize * sizeof(int));
+
+    if (ptr == NULL)
+    {
+        printf("Unable to allocate memory..!\n");
+        return -1;
+    }
+    else
+    {
+        printf("Memory gets allocated succesfully...\n");
+    }
+    
+    free(ptr);
+
+    return 0;
+}
