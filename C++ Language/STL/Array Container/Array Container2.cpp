fcbbbec0c794cf12aa39fd7cb3501a99326766ef
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-23 22:00:25 +0530

    Create Array Container2.cpp

---
+++ C++ Language/STL/Array Container/Array Container2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<array>
+using namespace std;
+
+int main()
+{
+    // It will take 0 automatically for other index. ie. {11, 21, 51, 0, 0}
+    // Stores 0 internally
+    array<int, 5> Arr = {11, 21, 51};
+
+    for (int i = 0; i < Arr[i]; i++)
+    {
+        cout<<Arr[i]<<" ";
+    }
+    
+
+    return 0;
+}
