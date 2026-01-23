765953a3e515ae657aab4a396e0da33a40f16b07
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-23 22:36:51 +0530

    Create Sort.cpp

---
+++ C++ Language/STL/Sort/Sort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>      // sort
+using namespace std;
+
+int main()
+{
+    vector<int> Arr = {5, 2, 4, 1, 3};
+
+    sort(Arr.begin(), Arr.end());
+
+    for (int i = 0; i < Arr[i]; i++)
+    {
+        cout<<Arr[i]<<" ";
+    }
+    
+
+    return 0;
+}
