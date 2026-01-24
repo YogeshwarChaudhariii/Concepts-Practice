6f5254334839f234573289a97dcf3a5039855e80
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-24 18:06:24 +0530

    Create sequence1.cpp

---
+++ C++ Language/Other Concepts/Paradigms/Sequence/sequence1.cpp
@@ -0,0 +1,15 @@
+// Code will execute line by line (sequentially)
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Jay Ganesh"<<endl;
+    cout<<"Jay Ganesh"<<endl;
+    cout<<"Jay Ganesh"<<endl;
+    cout<<"Jay Ganesh"<<endl;
+    cout<<"Jay Ganesh"<<endl;
+
+    return 0;
+}
