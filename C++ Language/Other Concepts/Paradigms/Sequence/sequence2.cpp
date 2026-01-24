528182175827b47ba9cd7827674cc99b4fbdfa2d
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-24 18:06:51 +0530

    Add files via upload

---
+++ C++ Language/Other Concepts/Paradigms/Sequence/sequence2.cpp
@@ -0,0 +1,15 @@
+// Code will execute line by line (sequentially)
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"1"<<endl;
+    cout<<"2"<<endl;
+    cout<<"3"<<endl;
+    cout<<"4"<<endl;
+    cout<<"5"<<endl;
+
+    return 0;
+}
---
+++ C++ Language/Other Concepts/Paradigms/Sequence/sequence3.cpp
@@ -0,0 +1,15 @@
+// Code will execute line by line (sequentially)
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"5"<<endl;
+    cout<<"4"<<endl;
+    cout<<"3"<<endl;
+    cout<<"2"<<endl;
+    cout<<"1"<<endl;
+
+    return 0;
+}
