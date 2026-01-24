e3710ec1f47c4d1aa015e77a30ed38331452fc6f
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-24 21:44:15 +0530

    Create StaticCast.cpp

---
+++ C++ Language/Other Concepts/Type Casting/Casting Operators/StaticCast.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c = 'a';
+  
+  	// Converting c from char to int
+  	int i = static_cast<int>(c);
+  
+	// Printing the converted value
+    cout << i;
+
+    return 0;
+}
