246d8961015951cac0058719e7052521f156b86a
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-02-16 00:38:44 +0530

    Update p1.cpp

--- C++ Language/Other Concepts/Array/p1.cpp
+++ C++ Language/Other Concepts/Array/p1.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int Arr[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>Arr[i];
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<Arr[i]<<" ";
+    }
+    
+
+    return 0;
+}
